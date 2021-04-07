990e113733dced98daac66788517a00cadc9efdd
Paul Litvinov <tackshine@gmail.com>
2021-04-07 16:47:23 +0300

    Pixel изменения :     Добавление методов :         ChekValue(double value) - проверка значеня на вхождение его в возможный диапазон.         ColorToChannel(double value) - трансформация значения цвета с вида от 0 до 1 в вид от 0 до 255.         Перегружен оператор присваивания от Pixel в  Color с учетом что потери данных не будет.         Перегружен оператор присваивания от Color в Pixel с учетом возможной потери данных.         Перегружен оператор умножения Pixel  на double, так как Pixel является вектором и перегрузка данного оператора оправдана для комфортного вида математической формулы.

--- Data/Pixel.cs
+++ Data/Pixel.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Drawing;
 
 namespace MyPhotoshop
@@ -29,16 +26,14 @@ namespace MyPhotoshop
             get => _r;
             set 
             {
-                if (value > 1 || value < 0) throw new ArgumentException("Color value must be beetween 0 and 1", nameof(value));
-                _r = value;
+                _r = CheckValue(value);
             }
         }
         public double G {
             get => _g;
             set
             {
-                if (value > 1 || value < 0) throw new ArgumentException("Color value must be beetween 0 and 1", nameof(value));
-                _g = value;
+                _g = CheckValue(value);
             }
         }
         public double B 
@@ -46,9 +41,79 @@ namespace MyPhotoshop
             get => _b;
             set
             {
-                if (value > 1 || value < 0) throw new ArgumentException("Color value must be beetween 0 and 1", nameof(value));
-                _b = value;
-             }
+                _b = CheckValue(value);
+            }
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Сhecking that the value is in the valid range 
+        /// </summary>
+        /// <param name="value">value for checking</param>
+        /// <returns>Checked value</returns>
+        /// <exception cref="ArgumentException"></exception>
+        private double CheckValue(double value)
+        {
+            if (value > 1 || value < 0) throw new ArgumentException("Color value must be beetween 0 and 1", nameof(value));
+            return value;
+        }
+
+        /// <summary>
+		/// Trim value if is out of valid range
+		/// </summary>
+		/// <param name="colorValue">Value of myltiply pixel color value on user parametr</param>
+		/// <returns>Valid value</returns>
+		private static double TrimColorValue(double colorValue)
+        {
+            if (colorValue < 0) colorValue = 0;
+            else if (colorValue > 1) colorValue = 1;
+            return colorValue;
+        }
+
+        /// <summary>
+        /// Convert color value from double to byte 
+        /// </summary>
+        /// <param name="value">Double color value</param>
+        /// <returns>Byte color value</returns>
+        /// <exception cref="ArgumentException"></exception>
+        public static byte ColorToChannel(double value)
+        {
+            if (value < 0 || value > 1)
+                throw new ArgumentException($"Wrong channel value {value} (the value must be between 0 and 1");
+            return (byte)(value * 255);
+        }
+
+        public static implicit operator Color(Pixel pixel)
+        {
+            return Color.FromArgb(
+                ColorToChannel(pixel._r),
+                ColorToChannel(pixel._g),
+                ColorToChannel(pixel._b)
+                );
+        }
+
+        public static explicit operator Pixel(Color color)
+        {
+            return new Pixel() 
+            { 
+                _r = color.R / 255,
+                _g = color.G / 255,
+                _b = color.B / 255
+            };
+        }
+
+        public static Pixel operator *(Pixel pixel, double value)
+        {
+            pixel.R = TrimColorValue(pixel._r * value);
+            pixel.G = TrimColorValue(pixel._g * value);
+            pixel.B = TrimColorValue(pixel._b * value);
+            return pixel;
+        }
+
+        public static Pixel operator *(double value, Pixel pixel)
+        {
+            return pixel * value;
         }
         #endregion
     }
